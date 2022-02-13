94bb4773719f87667330d528eeb704afa136a444
elifdiril <elifdiril6@gmail.com>
2022-02-13 21:02:26 +0300

    filter and copying tested

--- src/test/EmojiResults.test.js
+++ src/test/EmojiResults.test.js
@@ -2,16 +2,28 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
+import "@testing-library/jest-dom";
 
 describe("filterEmoji tests", () => {
-  let buttons, input;
+  let emojis, input;
   beforeEach(() => {
     render(<App />);
-    buttons = screen.getAllByTestId("emoji_row");
-    //input = screen.getByTestId("input");
+    emojis = screen.getAllByTestId("emoji_row");
+    input = screen.getByTestId("input");
   });
 
   test("Default Emoji lists size should be 20", () => {
-    expect(buttons.length).toEqual(20);
+    expect(emojis.length).toEqual(20);
+  });
+
+  test("Emoji list should lists for filter input", async () => {
+    const emojiText = "laughing";
+    userEvent.type(input, emojiText);
+    let laughEmoji = screen.getByText(/Laughing/i);
+    expect(laughEmoji).toBeInTheDocument();
+  });
+
+  test("Emoji must be copied when clicked", () => {
+    userEvent.click(emojis[0].parentElement);
   });
 });
